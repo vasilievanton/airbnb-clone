91fcf0662c34cbf3ff092da0ac8e6e16fcc11863
vasilievanton <vasilievanton36@gmail.com>
2022-07-29 16:38:04 +0300

    added conten about

--- src/components/pages/AboutPage.jsx
+++ src/components/pages/AboutPage.jsx
@@ -1,10 +1,11 @@
+import { Box, Typography } from '@mui/material';
 import React from 'react';
 
 const AboutPage = () => {
     return (
-        <div>
-            
-        </div>
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Typography sx={{ mt: 30, mb: 30 }}>Вот такой вот эбаут</Typography>
+        </Box>
     );
 };
 
--- src/components/pages/WishlistPage.jsx
+++ src/components/pages/WishlistPage.jsx
@@ -1,23 +1,27 @@
-import { Grid } from '@mui/material';
-import { Container } from '@mui/system';
+import { Grid, Typography } from '@mui/material';
+import { Box, Container } from '@mui/system';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import MyCard from '../MyCard';
 
-
 const WishlistPage = () => {
   const items = useSelector((state) => state.wishlist.items);
 
-    return (
-        <Container>
-
+  return (
+    <Container>
+      {items.length ? (
         <Grid container spacing={2} direction="row" justifyContent="flex-start" alignItems="stretch">
           {items.map((img) => (
             <MyCard img={img} key={img.id} />
           ))}
         </Grid>
-        </Container>
-    );
+      ) : (
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Typography sx={{ mt: 30, mb: 30 }}>Тут ничего нет. Добавь что-нибудь в избранное</Typography>
+        </Box>
+      )}
+    </Container>
+  );
 };
 
 export default WishlistPage;
